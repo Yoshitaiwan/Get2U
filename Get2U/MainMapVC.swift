346260cb5d91564dcbdf62eaef5cc68c473b4a40
Yoshi <Yoshi@Yoshis-MacBook-Air.local>
2017-04-05 14:00:38 +0800

    to compare against PokeMap Go sample code

--- Get2U/MainMapVC.swift
+++ Get2U/MainMapVC.swift
@@ -5,12 +5,24 @@
 //  Created by Yoshi on 1/4/17.
 //  Copyright © 2017 Yoshi. All rights reserved.
 //
+//  setting up MKMap https://www.youtube.com/watch?v=wU1XN-Gk1LM   learned how to set up constraint
+//
+// Taipei 101  -  25.033611, 121.565
+
+// Tutorial  https://www.youtube.com/watch?v=vWTrTHlt6Pk&list=PLZhNP5qJ2IA2z5ntmXoWhn8eCRstWfvu2  Ausome Tus
+
+
+
 
 import UIKit
 import MapKit
 
 class MainMapVC: UIViewController , MKMapViewDelegate, CLLocationManagerDelegate {
 
+    class CustomPointAnnotation: MKMapView{
+        var imageName: String!
+    }
+    
     @IBOutlet weak var mainMap: MKMapView!
     
     let locManager = CLLocationManager()
@@ -18,6 +30,7 @@ class MainMapVC: UIViewController , MKMapViewDelegate, CLLocationManagerDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         self.locManager.delegate = self
+        self.mainMap.delegate = self
         
         self.locManager.desiredAccuracy = kCLLocationAccuracyBest
         self.locManager.requestWhenInUseAuthorization()
@@ -36,12 +49,53 @@ class MainMapVC: UIViewController , MKMapViewDelegate, CLLocationManagerDelegate
     // location delgate method
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         let loc = locations.last
-        let userCenter = CLLocationCoordinate2D(latitude: loc!.coordinate.latitude, longitude: loc!.coordinate.longitude)
-        let region  = MKCoordinateRegion(center: userCenter, span:MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)) // zoon level
+        let userLoc = CLLocationCoordinate2D(latitude: loc!.coordinate.latitude, longitude: loc!.coordinate.longitude)
+        let region  = MKCoordinateRegion(center: userLoc, span:MKCoordinateSpan(latitudeDelta: 0.01, longitudeDelta: 0.01)) // zoon level
         self.mainMap.setRegion(region, animated: true)
-        self.locManager.stopUpdatingLocation()
-    }
     
+     // hard cording for testing 
+        
+        let coffeeLoc = CLLocationCoordinate2D(latitude: loc!.coordinate.latitude * 1.0001, longitude: loc!.coordinate.longitude)
+        
+        
+        
+     //   mainMap.removeAnnotations(mainMap.annotations)
+        
+        let coffeeAnnotation = MKPointAnnotation()
+        coffeeAnnotation.coordinate = coffeeLoc
+        coffeeAnnotation.title = "test coffee"
+        
+        let userAnnotation = MKPointAnnotation()
+        userAnnotation.coordinate = userLoc
+        userAnnotation.title = "test mee"
+   
+        
+        mainMap.addAnnotation(coffeeAnnotation)
+        mainMap.addAnnotation(userAnnotation)
+        
+        
+       self.locManager.stopUpdatingLocation()
+        
+    }
+  
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView?
+    {
+        if !(annotation is MKPointAnnotation){
+            print ("not registered as MKPOINT ANNOTATION")
+            return nil
+        }
+        var annotationView = mainMap.dequeueReusableAnnotationView(withIdentifier: "indentifier")
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation, reuseIdentifier: "identifer")
+            annotationView!.canShowCallout = true
+        }else{
+            annotationView?.annotation = annotation
+        }
+        
+        annotationView!.image = UIImage(named: "pickachu.png")
+        return annotationView
+    
+    }
     
     func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
         print("Error" + error.localizedDescription)
